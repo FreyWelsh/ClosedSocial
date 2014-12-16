7b856a46366183bd78d58ff107a12be9bde4aab2
twelsh212 <twelsh@bgsu.edu>
2014-12-16 01:17:42 -0500

    Added archive
    added archive, and main page modal. archive still needs some work, but
    should work out.

---
+++ js/process.php
@@ -0,0 +1,12 @@
+<?php
+	//http://www.krizna.com/jquery/jquery-ajax-form-submit-using-twitter-bootstrap-modal/
+	if (isset($_POST['name'])) {
+		$name = strip_tags($_POST['name']);
+		$email = strip_tags($_POST['Email']);
+		$sug = strip_tags($_POST['sug']);
+		echo "Name	=".$name."</br>"; 
+		echo "Email	=".$email."</br>"; 
+		echo "Message	=".$sug."</br>"; 
+		echo "<span class=\"label label-info\" >your message has been submitted .. Thanks you</span>";
+	}
+?>
